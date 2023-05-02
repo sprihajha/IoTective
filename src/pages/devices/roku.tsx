27342261704edbfdab18211b3d86f891fe85a6cb
Spriha Jha <jha.spriha@gmail.com>
2023-05-02 05:46:14 -0400

    Added support for Roku Streaming, Nest Thermostat, Philips Hue

---
+++ src/pages/devices/roku.tsx
@@ -0,0 +1,23 @@
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import { Device } from "../types/device";
+import DevicePage from "../../components/devicePage";
+
+const Roku = () => {
+  const router = useRouter();
+  const device = router.route.split("/").pop();
+  const [deviceData, setDeviceData] = useState<Device | null>(null);
+
+  useEffect(() => {
+    const fetchDeviceData = async () => {
+      const res = await fetch(`/api/devices/${device}`);
+      const data = await res.json();
+      setDeviceData(data);
+    };
+    fetchDeviceData();
+  }, [device]);
+
+  return <div>{deviceData && <DevicePage device={deviceData} />}</div>;
+};
+
+export default Roku;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,69 +1,115 @@
-import Link from "next/link";
-import Image from "next/image";
 import { motion } from "framer-motion";
+import DeviceCard from "../components/deviceCard";
 
-interface Device {
-  title: string;
-  image: string;
-  link: string;
-}
+import roombaImage from "../../public/roomba.png";
+import echoDotImage from "../../public/echo-dot.png";
+import ringImage from "../../public/ring_camera.png";
+import nestImage from "../../public/nest_thermostat.png";
+import hueImage from "../../public/philips_hue.png";
+import rokuImage from "../../public/roku.png";
+import { Cursor, Typewriter, useTypewriter } from "react-simple-typewriter";
+import { useState, useEffect } from "react";
 
-const devices: Device[] = [
+const devices = [
   {
     title: "Roomba",
-    image: "/roomba.jpg",
+    image: roombaImage,
     link: "/devices/roomba",
+    category: "Vacuum",
   },
   {
-    title: "Amazon Echo Dot",
-    image: "/echo_dot.jpg",
+    title: "Echo Dot",
+    image: echoDotImage,
     link: "/devices/echo-dot",
+    category: "Speaker",
   },
   {
     title: "Ring Camera",
-    image: "/ring_camera.jpg",
+    image: ringImage,
     link: "/devices/ring-camera",
+    category: "Camera",
+  },
+  {
+    title: "Roku Streaming",
+    image: rokuImage,
+    link: "/devices/roku",
+    category: "TV",
+  },
+  {
+    title: "Nest Thermostat",
+    image: nestImage,
+    link: "/devices/nest-thermostat",
+    category: "Temp",
+  },
+  {
+    title: "Philips Hue",
+    image: hueImage,
+    link: "/devices/philips-hue",
+    category: "Light",
   },
 ];
 
-const DeviceCard = ({ device }: { device: Device }) => (
-  <motion.div
-    whileHover={{ scale: 1.05 }}
-    whileTap={{ scale: 0.95 }}
-    className="flex flex-col items-center justify-center rounded-xl shadow-md p-8 m-4 transition-all duration-300 ease-in-out bg-fbb4ae"
-  >
-    <div className="relative">
-      <Image src={device.image} alt={device.title} width={40} height={40} />
-    </div>
-    <h2 className="text-2xl font-bold text-center mt-4 mb-2">{device.title}</h2>
-    <Link
-      className="bg-b3cde3 rounded-full px-8 py-3 text-lg font-bold transition-all duration-300 ease-in-out hover:bg-white hover:text-fbb4ae"
-      href={device.link}
-    >
-      Check it out
-    </Link>
-  </motion.div>
-);
-
-const Home = () => (
-  <div className="bg-ccebc5 min-h-screen">
-    <div className="container mx-auto px-4 py-12">
-      <h1 className="text-4xl text-center font-bold text-decbe4 mb-4">
-        IoTective
-      </h1>
-      <p className="text-xl text-center text-gray-800 mb-8">
-        Your guide to securing your IoT devices
-      </p>
-      <h2 className="text-2xl text-center font-bold text-gray-800 mb-4">
-        Select your device:
-      </h2>
-      <div className="flex flex-wrap justify-center">
-        {devices.map((device, index) => (
-          <DeviceCard key={index} device={device} />
-        ))}
-      </div>
-    </div>
-  </div>
-);
+const Home = () => {
+  return (
+    <>
+      <motion.div
+        className="bg-[#ccebc5] min-h-screen"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
+        <motion.div
+          className="container mx-auto px-4 py-12"
+          initial={{ y: -20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.2 }}
+        >
+          <motion.h1
+            className="text-5xl text-center font-bold text-decbe4 mb-4"
+            initial={{ y: -20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: 0.4 }}
+          >
+            IoTective
+          </motion.h1>
+          <motion.p
+            className="text-xl text-center text-gray-800 mb-8"
+            initial={{ y: -20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: 0.6 }}
+          >
+            <Typewriter
+              words={["We read the fine print, so you don't have to."]}
+              loop={1}
+              cursor
+              cursorStyle="_"
+              typeSpeed={60}
+              deleteSpeed={20}
+              delaySpeed={2000}
+            />
+          </motion.p>
+          <motion.h2
+            className="text-2xl text-center font-bold text-gray-800 mb-4"
+            initial={{ y: -20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: 0.8 }}
+          >
+            Select your device:
+          </motion.h2>
+          <motion.div
+            className="flex flex-wrap justify-center"
+            initial={{ y: -20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: 1 }}
+          >
+            {devices.map((device, index) => (
+              <DeviceCard key={index} device={device} />
+            ))}
+          </motion.div>
+        </motion.div>
+      </motion.div>
+    </>
+  );
+};
 
 export default Home;
